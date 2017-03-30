88ce8fc35e523c4e07be24412bf9b89d470274b6
fairuzfaria <fairuzfaria@yahoo.com>
2017-03-30 13:06:58 +0600

    Add files via upload

---
+++ system.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package genetic.algorithm;
+
+/**
+ *
+ * @author Anika
+ */
+import java.util.Scanner;
+public class GeneticAlgorithm {
+    public static void main(String[] args) {
+        { 
+        System.out.println("Iteration no 1");
+        System.out.println(" ");
+        Scanner sc = new Scanner(System.in);            
+        int count = 2;
+	people p = new people();
+        p.Generate();
+        System.out.println("How many iterations do you want?");
+        int iter = sc.nextInt();
+		for(int i=0;i<iter;i++)
+                {                   
+                    System.out.println("Iteration no"+" "+count);
+                    p.Generate2();
+                    count++;
+                }
+                p.Print();
+	}	
+    }   
+}
